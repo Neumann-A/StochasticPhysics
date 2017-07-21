1d0ca6ffb748b4d45c8913a392edcf8356c9577b
Alexander Neumann <alexander.neumann@hamburg.de>
2017-07-21 10:53:50 +0200

    Moved Project and Files with Test and becnhmarks into different folder.

---
+++ Benchmarks/Performance_ParticleSimulation/stdafx.cpp
@@ -0,0 +1,9 @@
+// stdafx.cpp : Quelldatei, die nur die Standard-Includes einbindet.
+// Performance_PartikelSimulation.pch ist der vorkompilierte Header.
+// stdafx.obj enthält die vorkompilierten Typinformationen.
+
+#include "stdafx.h"
+
+
+// TODO: Auf zusätzliche Header verweisen, die in STDAFX.H
+// und nicht in dieser Datei erforderlich sind.
---
+++ Benchmarks/Performance_ParticleSimulation/stdafx.h
@@ -0,0 +1,20 @@
+// stdafx.h: Includedatei für Standardsystem-Includedateien
+// oder häufig verwendete projektspezifische Includedateien,
+// die nur in unregelmäßigen Abständen geändert werden.
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <vector>
+#include <string>
+#include <type_traits>
+#include <utility>
+#include <tuple>
+#include <memory>
+
+
+// TODO: Hier auf zusätzliche Header, die das Programm erfordert, verweisen.
