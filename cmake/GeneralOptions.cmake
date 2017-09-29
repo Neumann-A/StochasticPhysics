0a33b42034bdcc9a2428119c89bb5bd087d8efb5
Alexander Neumann <Alexander.Neumann@hamburg.de>
2017-09-29 23:37:21 +0200

    Cleaned up and fixed most of the CMAKE issues!

--- cmake/GeneralOptions.cmake
+++ cmake/GeneralOptions.cmake
@@ -1,2 +1,10 @@
 OPTION(General_Benchmarks "Enable building of benchmarks" ON)
 OPTION(General_Tests "Enable building of tests" ON)
+
+if (CMAKE_COMPILER_IS_GNUCC)
+  option(General_Coverage "Enable coverage reporting for gcc/clang" FALSE)
+  
+    if (General_Coverage)
+    add_compile_options(--coverage -O0)
+  endif()
+endif()
