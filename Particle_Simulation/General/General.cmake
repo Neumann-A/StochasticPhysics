4869e55fe7c9d85d5ee40385ab8de176d22a1a99
Alexander Neumann <alexander.neumann@hamburg.de>
2019-04-04 12:47:57 +0200

    Fixing cmake scripts a bit.

--- Particle_Simulation/General/General.cmake
+++ Particle_Simulation/General/General.cmake
@@ -2,8 +2,7 @@ include_directories(General)
 set(CPP_FILES_General General/CommandOptions.cpp)
 set(H_FILES_General General/CommandOptions.h
                     General/GeneralDefines.h
-                    General/Setup.h
-                    General/SystemIncludes.h)
+                    General/Setup.h)
 source_group("General"        FILES ${CPP_FILES_General} ${INL_FILES_General} ${H_FILES_General})
 set(CPP_FILES ${CPP_FILES} ${CPP_FILES_General})
 set(INL_FILES ${INL_FILES} ${INL_FILES_General})
