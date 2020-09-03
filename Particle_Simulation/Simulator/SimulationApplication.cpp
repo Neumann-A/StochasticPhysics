bf2ea5320445f44fed9361e11b73385ff52a52fe
Alexander Neumann <Alexander.Neumann@hamburg.de>
2020-09-04 00:05:34 +0200

    fix makros

--- Particle_Simulation/Simulator/SimulationApplication.cpp
+++ Particle_Simulation/Simulator/SimulationApplication.cpp
@@ -124,7 +124,7 @@ int main(int argc, char** argv)
 				//Write additional Information to Result File. Application needs to be destroyed before! ():
 				switch (SimSettings.getResultSettings().getFileType())
 				{
-#ifdef ARCHIVE_HAS_MATLAB
+#ifdef SERAR_HAS_MATLAB
 				case Settings::IResultFileType::ResultFileType_MATLAB:
 				{
 					Archives::MatlabOutputArchive Ar{ SimSettings.getResultSettings().getFilepath(),Archives::MatlabOptions::update };
@@ -133,7 +133,7 @@ int main(int argc, char** argv)
 					break;
 				}
 #endif
-#ifdef ARCHIVE_HAS_HDF5
+#ifdef SERAR_HAS_HDF5
 				case Settings::IResultFileType::ResultFileType_HDF5:
 				{
 					Archives::HDF5_OutputArchive Ar{ SimSettings.getResultSettings().getFilepath() };
