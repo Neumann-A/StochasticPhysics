46e539ce274498086bb481dde06c1fba187fa483
Alexander Neumann <Alexander.Neumann@hamburg.de>
2020-09-24 00:04:37 +0200

    add empty anisotropy files

---
+++ Particle_Simulation/Problems/Anisotropy/MixedAnisotropy.h
@@ -0,0 +1,6 @@
+#ifndef INC_MixedAnisotropy_H
+#define INC_MixedAnisotropy_H
+
+#pragma once
+
+#endif
---
+++ Particle_Simulation/Properties/AnisotropyProperties.h
@@ -0,0 +1,25 @@
+
+///---------------------------------------------------------------------------------------------------
+// file:		AnisotropyProperties.h
+//
+// summary: 	Declares the properties for anisotropies
+//
+// Copyright (c) 2020 Alexander Neumann.
+//
+// author: Alexander Neumann
+// date: 08.09.2020
+
+#ifndef INC_AnisotropyProperties_H
+#define INC_AnisotropyProperties_H
+///---------------------------------------------------------------------------------------------------
+#pragma once
+
+namespace Properties {
+
+    class AnistropyProperties
+    {
+
+    }
+}
+
+#endif //INC_AnisotropyProperties_H
