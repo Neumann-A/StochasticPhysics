5dd5fc478c43d9c611b2e33b2475e13937033554
Alexander Neumann <Alexander.Neumann@hamburg.de>
2020-12-15 23:51:00 +0100

    add new files

---
+++ Particle_Simulation/Properties/Fields/All.hpp
@@ -0,0 +1,11 @@
+#ifndef SPHYS_PROPERTIES_FIELDS_ALL
+#define SPHYS_PROPERTIES_FIELDS_ALL
+
+#pragma once
+
+#include "Zero.hpp"
+#include "Sinusoidal.hpp"
+#include "Lissajous.hpp"
+#include "Rectangular.hpp"
+
+#endif SPHYS_PROPERTIES_FIELDS_ALL
