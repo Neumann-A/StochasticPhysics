025a28428bbe457804373a40a076949ffe9fcd21
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-06-09 23:04:58 +0200

    update cmakejson module

--- cmake/modules/FindCMakeJSON.cmake
+++ cmake/modules/FindCMakeJSON.cmake
@@ -1,34 +1,20 @@
-function(find_package_noise_args OUT_VAR)
-    set(PACKAGE_REQUIRED )
-    if(${CMAKE_FIND_PACKAGE_NAME}_FIND_REQUIRED)
-        set(PACKAGE_REQUIRED REQUIRED)
-    endif()
-    set(PACKAGE_QUITE)
-    if(${CMAKE_FIND_PACKAGE_NAME}_FIND_QUIETLY)
-        set(PACKAGE_QUITE QUITE)
-    endif()
-    set(${OUT_VAR} ${PACKAGE_QUITE} ${PACKAGE_REQUIRED})
-endfunction()
+include(FetchContent)
 
-find_package_noise_args(NOISE_ARGS)
+FetchContent_Declare(
+  CMakeJSON  
+  GIT_REPOSITORY https://github.com/Neumann-A/CMakeJSON.git
+  GIT_TAG        d52305f56b01069968dc575445ef735533e7a6c9
+)
 
-find_package(VCPKG ${NOISE_ARGS}) # Try to use VCPKG if available
-cmake_print_variables(VCPKG_ROOT VCPKG_TARGET_TRIPLET CMAKE_TOOLCHAIN_FILE VCPKG_INSTALLED_DIR CMAKE_PREFIX_PATH)
-if(VCPKG_INSTALLED_DIR AND NOT CMAKE_PREFIX_PATH MATCHES VCPKG_INSTALLED_DIR)
-    if(NOT CMakeJSON_DIR)
-        set(_FORCE FORCE)
-    endif()
-    set(CMakeJSON_DIR "${VCPKG_INSTALLED_DIR}/share/CMakeJSON" CACHE PATH "Path to CMakeJSON" ${_FORCE})
-    unset(_FORCE)
-    if(NOT GTest_DIR)
-        set(_FORCE FORCE)
-    endif()
-    set(GTest_DIR "${VCPKG_INSTALLED_DIR}/share/gtest" CACHE PATH "Path to GTest" ${_FORCE}) 
-    unset(_FORCE)
-    # Typically called before the first project() call
-    # as such the toolchain file is not yet loaded
+FetchContent_GetProperties(CMakeJSON)
+string(TOLOWER "CMakeJSON" lcName)
+
+if(NOT ${lcName}_POPULATED)
+    FetchContent_Populate(CMakeJSON)
+    set(CMakeJSON_INCLUDE_FILE "${${lcName}_SOURCE_DIR}/CMakeJSON/CMakeJSON.cmake")
+    set(CMakeJSON_FOUND)
+    include("${CMakeJSON_INCLUDE_FILE}")
 endif()
-cmake_print_variables(CMakeJSON_DIR GTest_DIR)
-find_package(CMakeJSON ${NOISE_ARGS} CONFIG)
 
-find_package_handle_standard_args(CMakeJSON CONFIG_MODE)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(CMakeJSON FOUND_VAR CMakeJSON_FOUND REQUIRED_VARS CMakeJSON_INCLUDE_FILE)
