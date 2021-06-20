e74e856e89ca7ebf9018547d6442f1bc098e1277
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-06-20 08:23:54 +0200

    remove unnecessary template arg

--- Particle_Simulation/Provider/PropertyProvider.h
+++ Particle_Simulation/Provider/PropertyProvider.h
@@ -26,42 +26,42 @@ enum class IProperty { ParticleProperties, AtomisticProperties};
 
 namespace Provider
 {
-	///-------------------------------------------------------------------------------------------------
-	/// <summary>	General Abstract Class for Providers  </summary>
-	///
-	/// <remarks>	Alexander Neumann, 05.06.2016. </remarks>
-	///
-	/// <seealso cref="T:IMATLABFileWriteable"/>
-	/// <seealso cref="T:IConfigFileAll{IGeneralProvider{prec}}"/>
-	///
-	/// <typeparam name="prec">	Floating point precission. </typeparam>
-	///-------------------------------------------------------------------------------------------------
-	template <typename prec>
-	class IGeneralProvider 
-	{
-		MY_INTERFACE(IGeneralProvider<prec>)
-		ALLOW_DEFAULT_COPY_AND_ASSIGN(IGeneralProvider<prec>)
-	private:
-		typedef IGeneralProvider<prec>		ThisClass;
-	protected:
-	public:
-		typedef prec						Precision;
-		//using IConfigFileAll<ThisClass>::WriteValuesToConfigFile;
-		//using IConfigFileAll<ThisClass>::GetValuesFromConfigFile;
-		//using IConfigFileAll<ThisClass>::createObjectFromConfigFile;
-		//using IMATLABFileWriteable::WriteValuesToMATLABFile;
+    ///-------------------------------------------------------------------------------------------------
+    /// <summary>	General Abstract Class for Providers  </summary>
+    ///
+    /// <remarks>	Alexander Neumann, 05.06.2016. </remarks>
+    ///
+    /// <seealso cref="T:IMATLABFileWriteable"/>
+    /// <seealso cref="T:IConfigFileAll{IGeneralProvider{prec}}"/>
+    ///
+    /// <typeparam name="prec">	Floating point precission. </typeparam>
+    ///-------------------------------------------------------------------------------------------------
+    template <typename prec>
+    class IGeneralProvider 
+    {
+        MY_INTERFACE(IGeneralProvider)
+        ALLOW_DEFAULT_COPY_AND_ASSIGN(IGeneralProvider)
+    private:
+        typedef IGeneralProvider<prec>		ThisClass;
+    protected:
+    public:
+        typedef prec						Precision;
+        //using IConfigFileAll<ThisClass>::WriteValuesToConfigFile;
+        //using IConfigFileAll<ThisClass>::GetValuesFromConfigFile;
+        //using IConfigFileAll<ThisClass>::createObjectFromConfigFile;
+        //using IMATLABFileWriteable::WriteValuesToMATLABFile;
 
-		///-------------------------------------------------------------------------------------------------
-		/// <summary>	Gets provided object. </summary>
-		///
-		/// <remarks>	Alexander Neumann, 05.06.2016. </remarks>
-		///
-		/// <typeparam name="T">	Generic type of Object Provided. </typeparam>
-		///
-		/// <returns>	The provided object. </returns>
-		///-------------------------------------------------------------------------------------------------
-		virtual Parameters::ParticleSimulationParameters<prec>	getProvidedObject() = 0;
-		virtual unsigned long long getNumberOfNecessarySimulations() const noexcept = 0;
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>	Gets provided object. </summary>
+        ///
+        /// <remarks>	Alexander Neumann, 05.06.2016. </remarks>
+        ///
+        /// <typeparam name="T">	Generic type of Object Provided. </typeparam>
+        ///
+        /// <returns>	The provided object. </returns>
+        ///-------------------------------------------------------------------------------------------------
+        virtual Parameters::ParticleSimulationParameters<prec>	getProvidedObject() = 0;
+        virtual unsigned long long getNumberOfNecessarySimulations() const noexcept = 0;
 
-	};
+    };
 }
