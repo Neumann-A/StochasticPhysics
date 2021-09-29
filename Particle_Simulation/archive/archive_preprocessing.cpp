f9949df041f251ffd5f09f967caa94cfc1c13c52
Alexander Neumann <neumann@imt.uni-luebeck.de>
2021-09-29 14:38:40 +0200

    apply changes from Matlab archive and don't include components headers.

--- Particle_Simulation/archive/archive_preprocessing.cpp
+++ Particle_Simulation/archive/archive_preprocessing.cpp
@@ -1,11 +1,9 @@
 #include "archive_preprocessing.hpp"
 #include "Settings/SimulationManagerSettings.h"
 #include <SerAr/Core/ArchiveHelper.h>
-// template Settings::SimulationManagerSettings<double>;
-// template Settings::SimulationManagerSettings<float>;
-
-// template Archives::LoadConstructor<Settings::SimulationManagerSettings<double>>;
-// template Archives::LoadConstructor<Settings::SimulationManagerSettings<float>>;
+#include <SerAr/AllArchiveIncludes.hpp>
+#include <SerAr/AllInputArchiveWrapper.hpp>
+#include <SerAr/AllOutputArchiveWrapper.hpp>
 
 #define SimulationManagerSettings_SAVE(archive, prec) \
 template void Settings::SimulationManagerSettings<prec>::save< archive  > ( archive & ar) const;
@@ -13,28 +11,24 @@ template void Settings::SimulationManagerSettings<prec>::save< archive  > ( arch
 template Settings::SimulationManagerSettings<prec>  Archives::LoadConstructor<Settings::SimulationManagerSettings<prec>>::construct< archive > ( Archives::InputArchive<archive> & ar); 
 
 #ifdef SERAR_HAS_CONFIGFILE
-#include <SerAr/ConfigFile/ConfigFile_Archive.h>
 SimulationManagerSettings_SAVE(Archives::ConfigFile_OutputArchive,double)
 SimulationManagerSettings_SAVE(Archives::ConfigFile_OutputArchive,float)
-//template Settings::SimulationManagerSettings<double> Archives::LoadConstructor<Settings::SimulationManagerSettings<double>>::construct< Archives::ConfigFile_InputArchive > ( Archives::InputArchive<Archives::ConfigFile_InputArchive> & ar);
 SimulationManagerSettings_CONSTRUCT(Archives::ConfigFile_InputArchive,double)
 SimulationManagerSettings_CONSTRUCT(Archives::ConfigFile_InputArchive,float)
 #endif
 
 #ifdef SERAR_HAS_MATLAB
-#include <SerAr/MATLAB/Matlab_Archive.h>
 static_assert(std::is_base_of_v< Eigen::EigenBase< Eigen::Matrix<double, 3, 1, 0> > , Eigen::Matrix<double, 3, 1, 0> >);
-static_assert(SerAr::ArchiveMemberSaveable<Eigen::Matrix<double, 3, 1, 0> &, Archives::MatlabOutputArchive>);
-static_assert(Archives::HasCreateMATLAB<Archives::MatlabOutputArchive, std::remove_cvref_t<Eigen::Matrix<double, 3, 1, 0> &>>);
+static_assert(SerAr::ArchiveMemberSaveable<Eigen::Matrix<double, 3, 1, 0> &, SerAr::Matlab_OutputArchive>);
+static_assert(SerAr::HasCreateMATLAB<SerAr::Matlab_OutputArchive, std::remove_cvref_t<Eigen::Matrix<double, 3, 1, 0> &>>);
 static_assert(stdext::is_eigen_type_v< std::remove_cvref_t<Eigen::Matrix<double, 3, 1, 0> &> >);
-SimulationManagerSettings_SAVE(Archives::MatlabOutputArchive,double)
-SimulationManagerSettings_SAVE(Archives::MatlabOutputArchive,float)
-SimulationManagerSettings_CONSTRUCT(Archives::MatlabInputArchive,double)
-SimulationManagerSettings_CONSTRUCT(Archives::MatlabInputArchive,float)
+SimulationManagerSettings_SAVE(SerAr::Matlab_OutputArchive,double)
+SimulationManagerSettings_SAVE(SerAr::Matlab_OutputArchive,float)
+SimulationManagerSettings_CONSTRUCT(SerAr::Matlab_InputArchive,double)
+SimulationManagerSettings_CONSTRUCT(SerAr::Matlab_InputArchive,float)
 #endif
 
 #ifdef SERAR_HAS_HDF5
-#include <SerAr/HDF5/HDF5_Archive.h>
 SimulationManagerSettings_SAVE(Archives::HDF5_OutputArchive,double)
 SimulationManagerSettings_SAVE(Archives::HDF5_OutputArchive,float)
 SimulationManagerSettings_CONSTRUCT(Archives::HDF5_InputArchive,double)
@@ -42,7 +36,6 @@ SimulationManagerSettings_CONSTRUCT(Archives::HDF5_InputArchive,float)
 #endif
 
 #ifdef SERAR_HAS_JSON
-#include <SerAr/JSON/JSON_Archives.hpp>
 SimulationManagerSettings_SAVE(SerAr::JSON_OutputArchive,double)
 SimulationManagerSettings_SAVE(SerAr::JSON_OutputArchive,float)
 SimulationManagerSettings_CONSTRUCT(SerAr::JSON_InputArchive,double)
@@ -50,7 +43,6 @@ SimulationManagerSettings_CONSTRUCT(SerAr::JSON_InputArchive,float)
 #endif
 
 #ifdef SERAR_HAS_TOML
-#include <SerAr/TOML/TOML_Archives.hpp>
 SimulationManagerSettings_SAVE(SerAr::TOML_OutputArchive,double)
 SimulationManagerSettings_SAVE(SerAr::TOML_OutputArchive,float)
 SimulationManagerSettings_CONSTRUCT(SerAr::TOML_InputArchive,double)
