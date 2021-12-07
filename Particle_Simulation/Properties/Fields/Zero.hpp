03c8683a833c44bdd7e7a1b8f608b7892d83f46f
Tobias Klemme <tob_k@arcor.de>
2021-12-07 14:49:07 +0100

    removed offsetfield

--- Particle_Simulation/Properties/Fields/Zero.hpp
+++ Particle_Simulation/Properties/Fields/Zero.hpp
@@ -15,17 +15,11 @@ namespace Properties::Fields
     template<typename prec>
     struct Zero : General<prec> {
         using ThisClass = Zero<prec>;
-
-        typedef Eigen::Matrix<prec, 3, 1>   Vec3D;
-        static const IField TypeOfField{ Properties::IField::Field_Zero };
-
-        Vec3D                   OffsetField{ Vec3D::Zero() };
-
+        static const IField TypeOfField{ Properties::IField::Field_Zero};
     };
 
     template<typename Precision, typename Archive>
     void serialize(Zero<Precision>& val, Archive& ar) {
-        ar(Archives::createNamedValue("Offset_Field",val.OffsetField));
     }
 
 }
