44a95cb9b468ae69d7accbfce9128dce702d5d1f
Tobias Klemme <tob_k@arcor.de>
2021-12-10 19:15:02 +0100

    changed overload

--- Particle_Simulation/SystemmatrixFileGeneration/SysMatFileGen.cpp
+++ Particle_Simulation/SystemmatrixFileGeneration/SysMatFileGen.cpp
@@ -30,24 +30,24 @@ int main(int argc, char** argv)
     using IField = Properties::IField;
 
     AppInputParams in_params(argc, argv);
-    Settings::SimulationManagerSettings<double> SimManSet{in_params.getAppParams()};
+    Settings::SimulationManagerSettings<PREC> SimManSet{in_params.getAppParams()};
 
     auto &var = SimManSet.getFieldProperties().fieldproperties.variant;
 std::visit(overload{
         [](SettingsFileGen::field_enum_property_mapping<IField::Field_Zero>::type){
                     std::puts("Zero field not supportet");},
-        [&in_params](SettingsFileGen::field_enum_property_mapping<IField::Field_Constant>::type&){
-                    SettingsFileGen::SimFileGen<SettingsFileGen::field_enum_property_mapping<IField::Field_Constant>::type>(in_params);},
-        [&in_params](SettingsFileGen::field_enum_property_mapping<IField::Field_Sinusoidal>::type&){
-                    SettingsFileGen::SimFileGen<SettingsFileGen::field_enum_property_mapping<IField::Field_Sinusoidal>::type>(in_params);},
-        [&in_params](SettingsFileGen::field_enum_property_mapping<IField::Field_Lissajous>::type&){
-                    SettingsFileGen::SimFileGen<SettingsFileGen::field_enum_property_mapping<IField::Field_Lissajous>::type>(in_params);},
-        [&in_params](SettingsFileGen::field_enum_property_mapping<IField::Field_Triangular>::type&){
-                    SettingsFileGen::SimFileGen<SettingsFileGen::field_enum_property_mapping<IField::Field_Triangular>::type>(in_params);},
-        [&in_params](SettingsFileGen::field_enum_property_mapping<IField::Field_Rectangular>::type&){
-                    SettingsFileGen::SimFileGen<SettingsFileGen::field_enum_property_mapping<IField::Field_Rectangular>::type>(in_params);},
-        [&in_params](SettingsFileGen::field_enum_property_mapping<IField::Field_Sinc>::type&){
-                    SettingsFileGen::SimFileGen<SettingsFileGen::field_enum_property_mapping<IField::Field_Sinc>::type>(in_params);}
+        [&in_params,&SimManSet](SettingsFileGen::field_enum_property_mapping<IField::Field_Constant>::type&){
+                    SettingsFileGen::SimFileGen<SettingsFileGen::field_enum_property_mapping<IField::Field_Constant>::type>(in_params,SimManSet);},
+        [&in_params,&SimManSet](SettingsFileGen::field_enum_property_mapping<IField::Field_Sinusoidal>::type&){
+                    SettingsFileGen::SimFileGen<SettingsFileGen::field_enum_property_mapping<IField::Field_Sinusoidal>::type>(in_params,SimManSet);},
+        [&in_params,&SimManSet](SettingsFileGen::field_enum_property_mapping<IField::Field_Lissajous>::type&){
+                    SettingsFileGen::SimFileGen<SettingsFileGen::field_enum_property_mapping<IField::Field_Lissajous>::type>(in_params,SimManSet);},
+        [&in_params,&SimManSet](SettingsFileGen::field_enum_property_mapping<IField::Field_Triangular>::type&){
+                    SettingsFileGen::SimFileGen<SettingsFileGen::field_enum_property_mapping<IField::Field_Triangular>::type>(in_params,SimManSet);},
+        [&in_params,&SimManSet](SettingsFileGen::field_enum_property_mapping<IField::Field_Rectangular>::type&){
+                    SettingsFileGen::SimFileGen<SettingsFileGen::field_enum_property_mapping<IField::Field_Rectangular>::type>(in_params,SimManSet);},
+        [&in_params,&SimManSet](SettingsFileGen::field_enum_property_mapping<IField::Field_Sinc>::type&){
+                    SettingsFileGen::SimFileGen<SettingsFileGen::field_enum_property_mapping<IField::Field_Sinc>::type>(in_params,SimManSet);}
 },var);
         return 0;
 }
