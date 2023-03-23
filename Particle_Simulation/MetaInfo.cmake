3845c7f1480c13989d66bf0064c8617a27bf22fa
Alexander Neumann <alexander.neumann@hamburg.de>
2023-03-23 12:10:05 +0100

    try enabling builds without git

--- Particle_Simulation/MetaInfo.cmake
+++ Particle_Simulation/MetaInfo.cmake
@@ -6,6 +6,7 @@ set(GIT_WORKING_DIR "${CMAKE_CURRENT_SOURCE_DIR}")
 string(TIMESTAMP PROJECT_DATE "%Y-%m-%d" UTC)
 string(TIMESTAMP PROJECT_TIME "%H:%M:%S" UTC)
 
+if(Git_FOUND)
 execute_process(COMMAND "${GIT_EXECUTABLE}" rev-parse --abbrev-ref HEAD
                 WORKING_DIRECTORY "${GIT_WORKING_DIR}"
                 OUTPUT_VARIABLE PROJECT_GIT_BRANCH
@@ -21,5 +22,13 @@ execute_process(COMMAND "${GIT_EXECUTABLE}" describe --always --dirty --tags
                 OUTPUT_VARIABLE PROJECT_GIT_DESCRIBE
                 COMMAND_ERROR_IS_FATAL ANY
                 OUTPUT_STRIP_TRAILING_WHITESPACE)
-
+else()
+    set(PROJECT_GIT_DESCRIBE "Without-Git" CACHE STRING "")
+    if(NOT PROJECT_GIT_SHA)
+        message(FATAL_ERROR "Please manually set PROJECT_GIT_SHA")
+    endif()
+    if(NOT PROJECT_GIT_BRANCH)
+        message(FATAL_ERROR "Please manually set PROJECT_GIT_BRANCH")
+    endif()
+endif()
 configure_file("meta/metainfo.in.cpp" "${TARGET_DIR}/meta/metainfo.cpp" @ONLY)
